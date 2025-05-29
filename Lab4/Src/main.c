560e93e0ed50b4ae2c271d9d3a9467d0134535b2
Jake <jakeclarey@gmail.com>
2025-05-29 19:32:27 -0400

    beginning lab4

---
+++ Lab4/Src/main.c
@@ -0,0 +1,20 @@
+/* Author: Jacob Clarey
+ * Instructor: Dr. Parikh
+ * Date: 5/29/2025
+ * Description:
+ *
+ * Notes:
+ */
+
+#include "blink.h"
+#include <stdio.h>
+#include <stm32wb55xx.h>
+
+int main(void) {
+
+  while (1) {
+
+  } // while(1)
+} // main(void)
+
+/* EOF */
